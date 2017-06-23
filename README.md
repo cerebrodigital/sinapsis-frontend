3aab5fc1f60745d140bf0df349feaf01f1b9613f
esteban <estecaes@gmail.com>
2017-06-23 09:24:25 -0500

    express with pug and vue

--- README.md
+++
@@ -1,2 +0,0 @@
-# sinapsis-frontend
-Sinapsis Frontend
--- src/pages/pages.ts
+++
@@ -1,9 +0,0 @@
-import { ContentPage } from './content/content';
-import { LoginPage } from './login/login';
-
-// The page the user lands on after opening the app and without a session
-export const FirstRunPage = LoginPage;
-
-// The main page the user will see as they use the app over a long period of time.
-// Change this if not using tabs
-export const MainPage = ContentPage;
